4d327ff5ed15986570f3a8398f1c88a09d5bfb3d
Hadi <hadi.effatian@gmail.com>
2018-04-06 11:53:28 +0200

    Created Project

---
+++ src/main/java/at/refugeescode/mp11_pirates_of_the_code_bean_3/Mp11PiratesOfTheCodeBean3Application.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp11_pirates_of_the_code_bean_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp11PiratesOfTheCodeBean3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp11PiratesOfTheCodeBean3Application.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp11_pirates_of_the_code_bean_3/Mp11PiratesOfTheCodeBean3ApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp11_pirates_of_the_code_bean_3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp11PiratesOfTheCodeBean3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
